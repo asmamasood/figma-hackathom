0723ad55df000dfd01b875260c295d2a03104b6d
asmamasood <asmasood2@gmail.com>
2025-01-19 11:53:08 +0500

    Added all files

---
+++ src/app/Admin/responsive.tsx
@@ -0,0 +1,208 @@
+import React from "react";
+import Image from "next/image";
+const responsive = () => { 
+    return (
+        <>
+{/* Main Content */}
+      <main className="grid-cols-1 lg:hidden ">
+        {/* Details Rental Section */}
+        <div className="grid grid-cols-1 gap-4">
+          <div className="col-span-2  bg-white p-6 rounded-lg shadow">
+            <h2 className="text-lg font-semibold mb-4">Details Rental</h2>
+          
+              <Image
+                src={"/Maps.png"}
+                alt="map Image"
+                width={295}
+                height={275}
+                className="w-full rounded"/>
+              
+            
+            <div className="flex items-center space-x-4 mb-4"
+            >
+              <Image
+                src="/image8.png"
+                alt="Car Image"
+                width={116}
+                height={64}
+                className="rounded"style={{ backgroundImage: "url('/blueBack.jpg')" }}
+              />
+              <div>
+                <h3 className="text-xs lg:text-xl font-bold">Nissan GT–R</h3>
+                <p className="text-gray-500 text-xs lg:text-lg">Sport Car</p>
+              </div>
+            </div>
+            <div className="grid-cols-1  gap-4">
+              {/* Pick-Up Section */}
+  <div className="">
+    <div className="flex items-center gap-2 mb-4">
+      <span className="w-3 h-3 bg-blue-500 rounded-full"></span>
+      <p className="font-semibold text-gray-700 text-lg">Pick-Up</p>
+    </div>
+    <div className="flex flex-col">
+      {/* Locations  */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Locations</label>
+        <select className="w-40 bg-white text-[#90A3BF]">
+          <option>Kota Semarange</option>
+        </select>
+      </div>
+      {/* Date  */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Date</label>
+        <select className="w-40 bg-white text-[#90A3BF] ">
+          <option>20july2022</option>
+        </select>
+      </div>
+      {/* Time  */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Time</label>
+        <select className="w-40 bg-white text-[#90A3BF] ">
+          <option>07:00</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+ 
+
+  {/* Drop-Off Section */}
+  <div className="grid-cols-1">
+    <div className="flex items-center gap-2 mb-4">
+      <span className="w-3 h-3 bg-blue-500 rounded-full"></span>
+      <p className="font-semibold text-gray-700 text-lg">Drop-Off</p>
+    </div>
+    <div className=" gap-4">
+      {/* Locations Dropdown */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Locations</label>
+        <select className="w-40 bg-white text-[#90A3BF] ">
+          <option>Kota Semarange </option>
+        </select>
+      </div>
+      {/* Date Dropdown */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Date</label>
+        <select className="w-40 bg-white text-[#90A3BF] ">
+        <option>21july2022</option>
+        </select>
+      </div>
+      {/* Time Dropdown */}
+      <div className="flex flex-col">
+        <label className="block text-sm font-bold text-gray-600 mb-1">Time</label>
+        <select className="w-40 bg-white text-[#90A3BF] ">
+          <option>01:00</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</div>
+  <div className="flex items-center">
+         <div className="mt-4 text-lg font-bold">
+              Total Rental Price: 
+              <p className="  text-[#90A3BF] text-sm">Overall price and includes rental discount</p></div>
+               <span className="text-[#1A202C] text-3xl font bold">$80.00</span>
+            </div>
+          </div>
+          <div className="space-y-1"> 
+  {/* Top 5 Car Rental */}
+  <div className="w-[327px] h-[300px] bg-white p-6 rounded-lg shadow">
+    <h2 className="text-lg font-semibold mb-4">Top 5 Car Rental</h2>
+    <div className=" items-center justify-center h-40">
+      <Image
+        src="/Top 5 Car Rental.png"
+        alt="Circle Image"
+        width={524}
+        height={324}
+      />
+    </div>
+  </div>
+
+  {/* Recent Transaction */}
+  <div className="w-[327px] h-[352px] bg-white p-6 rounded-lg shadow">
+    <div className="w-full h-[24px] flex justify-between mb-4">
+      <h2 className="text-lg font-semibold">Recent Transaction</h2>
+      <a href="#" className="text-blue-600 text-sm">
+        View All
+      </a>
+    </div>
+    <ul>
+      <li className="w-full h-[70px] flex justify-between items-center py-2 border-b">
+        <div className="flex items-center">
+          <Image
+            src="/image8.png"
+            alt="Nissan GT-R"
+            width={72}
+            height={24}
+            className="mr-4"
+          />
+          <div>
+            <p className="font-semibold">Nissan GT-R</p>
+            <p className="text-gray-500 text-sm">Sport Car</p>
+          </div>
+        </div><div>
+        <p className="text-gray-500 text-sm">20 July</p>
+        <p className="font-semibold">$80.00</p></div>
+      </li>
+      <li className="w-full h-[70px] flex justify-between items-center py-2 border-b">
+        <div className="flex items-center">
+          <Image
+            src="/image7.png" 
+            alt="Koenigsegg"
+            width={72}
+            height={24}
+            className="mr-4"
+          />
+          <div>
+            <p className="font-semibold">Koenigsegg</p>
+            <p className="text-gray-500 text-sm">Sport Car</p>
+            
+          </div>
+        </div>
+        <div><p className="text-gray-500 text-sm">19 July</p>
+        <p className="font-semibold">$99.00</p></div>
+      </li>
+      <li className="w-full h-[70px] flex justify-between items-center py-2 border-b">
+        <div className="flex items-center">
+          <Image
+            src="/Car (2).png" 
+            alt="Rolls-Royce"
+            width={72}
+            height={24}
+            className="mr-4"
+          />
+          <div>
+            <p className="font-semibold">Rolls-Royce</p>
+            <p className="text-gray-500 text-sm">Sport Car</p>
+          </div>
+        </div>
+        <div>
+        <p className="text-gray-500 text-sm">18 July</p>
+        <p className="font-semibold">$96.00</p></div>
+      </li>
+      <li className="w-full h-[70px] flex justify-between items-center py-2">
+        <div className="flex items-center">
+          <Image
+            src="/Car (5).png" 
+            alt="CR-V"
+            width={72}
+            height={24}
+            className="mr-4"
+          />
+          <div>
+            <p className="font-semibold">CR-V</p>
+            <p className="text-gray-500 text-sm">SUV</p>
+          </div>
+        </div> <div>
+        <p className="text-gray-500 text-sm">17 July</p>
+        <p className="font-semibold">$80.00</p></div>
+      </li>
+    </ul>
+  </div></div></div>
+      </main>
+</>
+)}
+export default responsive;
+
+
+
---
+++ src/app/Detail/page.tsx
@@ -0,0 +1,140 @@
+import Image from "next/image";
+import React from "react";
+import Reviews from "./reviews";
+
+
+export default function DetailPage() {
+  return (
+    
+<div className=" grid-col-1 lg:grid-col-3 max-w-7xl mx-auto mt-8 gap-6">
+
+      {/* Sidebar */}
+    
+<div className="flex">
+    
+      <aside className="hidden lg:w-[360px] h-[520px] bg-white p-4 rounded-lg shadow-md sm:hidden lg:block">
+        <div>
+          <h3 className="w-[176px]  font-semibold mb-4">Type</h3>
+          <ul>
+            <li className="mb-2 w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" defaultChecked className="mr-2 " /> Sport (10)
+            </li>
+            <li className="mb-2 w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" defaultChecked className=" mr-2" /> SUV (12)
+            </li>
+            <li className="mb-2 w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" className=" mr-2" /> MPV (16)
+            </li>
+            <li className="mb-2 w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" className="mr-2" /> Sedan (20)
+            </li>
+            <li className="mb-2 w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" className=" mr-2" /> Coupe (14)
+            </li>
+            <li className="mb-2w-[176px] h-[24px] gap-[8px]">
+              <input type="checkbox" className="mr-2 " /> Hatchback (14)
+            </li>
+          </ul>
+        </div>
+        <div className="mt-6 w-[176px] h-[180px] ">
+          <h3 className="font-semibold mb-4">Capacity</h3>
+          <ul>
+            <li className="mb-2">
+              <input type="checkbox" defaultChecked className="mr-2" /> 2 Person (10)
+            </li>
+            <li className="mb-2">
+              <input type="checkbox" className="mr-2" /> 4 Person (14)
+            </li>
+            <li className="mb-2">
+              <input type="checkbox" className="mr-2" /> 6 Person (12)
+            </li>
+            <li className="mb-2">
+              <input type="checkbox" defaultChecked className="mr-2" /> 8 or More (16)
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          <h3 className="font-semibold leading-[15.2px] tracking-[-2%] ">Price</h3>
+          <input type="range" min="0" max="100" defaultValue="80" className="w-full" />
+          <p className=" text-gray-600">Max. $100.00</p>
+        </div>
+      </aside>
+
+   <div className="item center lg:flex">
+      {/* Main Content */}
+      <main className="flex-1 justify-between items-center w-[327px] lg:w-[492px] h-[232] lg:h-[500px]">
+        <div className="bg-blue-600 text-white p-6 rounded-lg shadow-md" style={{ backgroundImage: "url('/blueBack.jpg')" }}>
+         
+          <h1 className="w-[372px] h-[75px]   tracking[-3%] font-Plus Jakarta Sans text-lg font-bold">
+            Sports car with the best <br /> design and acceleration</h1>
+          <p className=" w-[284px] h-[48px] text-sm">Safety and comfort while driving a futuristic and elegant sports car</p>
+          <Image
+            src="/image 8 (1).png"
+            alt="Car"
+            className="w-[190px] lg:w-[380px] h-[60px] lg:h-[120px]   left-[436px] hue-rotate-180 rounded-lg"
+          /> </div>
+          <div className="flex gap-2 mt-4 mb-4" >
+            <div className="w-[96px] lg:w-[190px] h-[64px] lg:h-[124px] rounded pt-2 lg:pt-10 pl-2 lg:pl-6"style={{ backgroundImage: "url('/blueBack.jpg')"}}>
+            <Image 
+              src="/car (18).png"
+              alt="car"
+              className="w-[116px] h-[36px] top-[36px] left-[124px] hue-rotate-180  "
+            />
+            </div>
+            <Image
+              src="/View 2.png"
+              alt="Interior"
+              className="w-[96px] lg:w-[190px] h-[64px] lg:h-[124px] rounded-lg"
+            />
+             <Image
+              src="/View 3.png"
+              alt="Interior"
+              className="w-[96px] lg:w-[190px] h-[64px] lg:h-[124px] rounded-lg "
+            />
+          
+        </div>
+
+        
+      </main>
+      
+
+      {/* Right Section */}
+      <aside className=" w-fll lg:w-1/4 h-[500px] bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold mb-2">Nissan GT – R</h2> <Image src="/Review star.png" alt="stars" className="w-[108px] h-[20px]"/>
+        <p className="text-gray-600 mb-4">
+          NISMO has become the embodiment of Nissans outstanding performance, inspired by the most
+          unforgiving proving ground, the race track.
+        </p>
+        <ul className="mb-4 mt-9 ">
+          <li className="flex justify-between ">
+            <span>Type Car:</span> <span>Sport</span>
+            <span>Capacity:</span> <span>2 Person</span>
+          </li>
+          <li className="flex justify-between">
+            <span>Steering:</span> <span>Manual</span>
+            <span>Gasoline:</span> <span>70L</span>
+          </li>
+          
+        </ul>
+        <div className="flex items-center gap-4 mt-20">
+          <h3 className="text-xl font-bold text-black">$80.00/day</h3>
+          <p className="line-through text-gray-400">$100.00</p>
+        </div>
+        <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md">
+          Rent Now
+        </button>
+        
+         </aside> </div> </div>
+         <div className="flex-col">  <Reviews/> </div>
+         
+      
+      
+      </div>
+
+
+
+
+ 
+  )}
+
+ 
